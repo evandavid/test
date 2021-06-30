0341645de1646405021f514727e60ece63f19afc
Evan <evan.kristian@polyrific.com>
2021-06-30 19:19:21 +0700

    fix npm vulnerabilities

--- components/modivated/modivated.js
+++ components/modivated/modivated.js
@@ -6,7 +6,7 @@ import CoverImage from 'public/img/modivated/cover-image.png';
 import BtnCommon from 'components/commons/buttons/BtnCommon';
 import * as Styled from './styled';
 import Link from 'next/link';
-import Tooltip from 'components/commons/Tooltip';
+import Tooltip from '../commons/Tooltip';
 
 const modivated = () => (
   <Styled.ModivatedSection>
