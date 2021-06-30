91bc31a7458cfefb3ca6171509a25e8c96bd4c08
Evan <evan.kristian@polyrific.com>
2021-06-30 20:25:16 +0700

    update footer disclosure

--- components/layout/Footer/Disclosure/index.js
+++ components/layout/Footer/Disclosure/index.js
@@ -4,12 +4,17 @@ import * as Styled from './styled';
 import messages from './messages';
 
 const Disclosure = ({ intl }) => {
-  const translated1 = messages.disclosureParagraph1
-    ? intl.formatMessage(messages.disclosureParagraph1)
-    : '';
-  const translated2 = messages.disclosureParagraph2
-    ? intl.formatMessage(messages.disclosureParagraph2)
-    : '';
+  const translated1 = intl.formatMessage(messages.disclosureParagraph1, {
+    link1: '<a href="/legal/terms-of-use">Terms of Use</a>',
+    link2: `<a href="/legal/privacy-policy">Privacy Policy</a>`,
+  });
+
+  const translated2 = intl.formatMessage(messages.disclosureParagraph2, {
+    link1: '<a href="https://www.finra.org/#/" target="_blank" rel="noopener noreferrer">FINRA</a>',
+    link2: '<a href="https://www.sipc.org/" target="_blank" rel="noopener noreferrer">SIPC</a>',
+    link3:
+      '<a href="https://brokercheck.finra.org/firm/summary/154559" target="_blank" rel="noopener noreferrer" aria-label="Open Finra BrokerCheck page">here</a>',
+  });
 
   return (
     <Styled.Container>
--- components/layout/Footer/Disclosure/messages.js
+++ components/layout/Footer/Disclosure/messages.js
@@ -4,12 +4,12 @@ export default defineMessages({
   disclosureParagraph1: {
     id: 'footer.disclosureParagraph1',
     defaultMessage:
-      'This site is operated by Modiv “Company”, which is not a registered broker-dealer. “Company” does not give investment advice, endorsement, analysis or recommendations with respect to any securities. By accessing this site and any pages thereof, you agree to be bound by the <a href="/legal/terms-of-use">Terms of Use</a> and <a href="/legal/privacy-policy">Privacy Policy</a>.',
+      'This site is operated by Modiv “Company”, which is not a registered broker-dealer. “Company” does not give investment advice, endorsement, analysis or recommendations with respect to any securities. By accessing this site and any pages thereof, you agree to be bound by the {link1} and {link2}',
   },
   disclosureParagraph2: {
     id: 'footer.disclosureParagraph2',
     defaultMessage:
-      'Securities offered through North Capital Private Securities, Member <a href="https://www.finra.org/#/" target="_blank" rel="noopener noreferrer">FINRA</a>/<a href="https://www.sipc.org/" target="_blank" rel="noopener noreferrer">SIPC</a>, located at 623 East Ft. Union Blvd, Suite 101, Salt Lake City, UT 84047. NCPS does not make investment recommendations and no communication, through this website or in any other medium should be construed as a recommendation for any security offered on or off this investment platform. You can review the brokercheck for NCPS <a href="https://brokercheck.finra.org/firm/summary/154559" target="_blank" rel="noopener noreferrer" aria-label="Open Finra BrokerCheck page">here</a>.',
+      'Securities offered through North Capital Private Securities, Member {link1}/{link2}, located at 623 East Ft. Union Blvd, Suite 101, Salt Lake City, UT 84047. NCPS does not make investment recommendations and no communication, through this website or in any other medium should be construed as a recommendation for any security offered on or off this investment platform. You can review the brokercheck for NCPS {link3}.',
   },
   disclosureParagraph3: {
     id: 'footer.disclosureParagraph3',
