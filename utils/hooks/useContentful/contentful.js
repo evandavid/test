87042a6523bc09a811c2adb27fe35f5accf53128
Evan <evan.kristian@polyrific.com>
2021-07-01 11:42:58 +0700

    fix: avoid serving legacy javascript to modern browsers

--- utils/hooks/useContentful/contentful.js
+++ utils/hooks/useContentful/contentful.js
@@ -1,8 +1,10 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { createClient } from 'contentful';
 
-export default ({ space, accessToken, environment, host }) => {
+const contentfulClient = ({ space, accessToken, environment, host }) => {
   return host
     ? createClient({ space, accessToken, environment, host })
     : createClient({ space, accessToken, environment });
 };
+
+export default contentfulClient;
